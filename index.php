619c16693c01d0ff53f2f70a49934e3c8a62550f
Toxe <toxe@toxe.net>
2025-06-30 12:01:38 +0200

    Enable Twig debug mode and disable caching

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require(__DIR__ . '/src/router.php');
 
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
 $twig = new \Twig\Environment($loader, [
+    'debug' => true,
     'cache' => __DIR__ . '/cache',
 ]);
 
